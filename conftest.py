432eea1efc503a92f964215d23659ef0f505552e
Shah rahman <toshah.rahman@gmail.com>
2022-04-01 21:55:08 +0300

    pytest fun 11

--- conftest.py
+++ conftest.py
@@ -1,23 +1,24 @@
 from pytest import fixture
 
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.utils import ChromeType
+from selenium.webdriver.edge.service import Service
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
 from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.firefox.options import Options
 from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.utils import ChromeType
 
 
-@fixture(scope='session')
+@fixture(scope='function')
 def chrome_browser():
     c = Options()
     # passing headless parameter
     c.add_argument("--headless")
-    browser = webdriver.Chrome(service=Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()),
+    browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()),
                                options=c)
-    yield browser
+    return browser
 
 
 @fixture(scope='function')
@@ -26,4 +27,22 @@ def firefox_browser():
     # passing headless parameter
     c.add_argument("--headless")
     browser = webdriver.Firefox(service=Service(GeckoDriverManager().install()), options=c)
+    yield browser
+
+
+@fixture(scope='function')
+def edge_browser():
+    c = Options()
+    # passing headless parameter
+    c.add_argument("--headless")
+    browser = webdriver.Edge(service=Service(EdgeChromiumDriverManager().install()), options=c)
+    yield browser
+
+
+@fixture(scope='function')
+def brave_browser():
+    c = Options()
+    # passing headless parameter
+    c.add_argument("--headless")
+    browser = webdriver.Chrome(service=Service(ChromeDriverManager(chrome_type=ChromeType.BRAVE).install()),options=c)
     yield browser
