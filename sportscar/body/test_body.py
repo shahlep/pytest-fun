b312294ac96567135c24bd10a0f4f0ceafba2107
Shah rahman <toshah.rahman@gmail.com>
2022-04-02 11:10:10 +0300

    pytest fun 15

--- sportscar/body/test_body.py
+++ sportscar/body/test_body.py
@@ -6,7 +6,7 @@ class BodyTest:
 
     @mark.smoke
     @mark.ui
-    def test_can_navigate_to_playwright_docs(self, chrome_browser):
-        chrome_browser.get("https://playwright.dev/python/docs/intro")
-        chrome_browser.close()
+    def test_can_navigate_to_playwright_docs(self, firefox_browser):
+        firefox_browser.get("https://playwright.dev/python/docs/intro")
+        firefox_browser.close()
         assert True
